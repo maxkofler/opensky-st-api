d40dde6d4954397897b3df1582307a33a5232eee
Max Kofler <kofler.max.dev@gmail.com>
2023-02-17 10:37:36 +0100

    main: Added CORS argument

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from webserver import webserver
 from webserver import endpoints
 from webserver import usermanager
 import blog
+import argparse
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wildcard-cors", help="Lets the webserver send wildcard CORS headers for testing purposes", action="store_true")
+    args = parser.parse_args()
 
     # Setup the configuration and logger
     blog.info("Setting up webserver configuration..")
     webserver.WEB_CONFIG["logger_function_debug"] = blog.debug
     webserver.WEB_CONFIG["logger_function_info"] = blog.web_log
     webserver.WEB_CONFIG["web_debug"] = True
+    webserver.WEB_CONFIG["send_cors_headers"] = args.wildcard_cors
+
+    if (args.wildcard_cors):
+        blog.warn("WARNING: You are sending wildcard CORS headers, this should not be used in production!")
 
     # Load the user file for the first time
     blog.info("Loading user file..")
