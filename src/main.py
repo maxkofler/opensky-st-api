f3e2ea30830c88381e19333f57c8a2c8c00ab41f
Max Kofler <kofler.max.dev@gmail.com>
2023-03-02 16:35:16 +0100

    Add Plane and Entry fetching

--- src/main.py
+++ src/main.py
@@ -5,6 +5,92 @@ from branchweb import webauth
 import blog
 import argparse
 
+import threading
+import asyncio
+import websockets
+import json
+import dateutil.parser as dparser
+import os
+
+class Entry:
+    def __init__(self, json):
+        self.addr = int(json["Icao_addr"])
+        self.tail = str(json["Tail"]).strip()
+        self.alt = round(int(json["Alt"]) * 0.305, 0)
+        self.speed = round((int(json["Speed"]) * 1.852) / 3.6, 0)
+        self.track = int(json["Track"])
+        self.lat = float(json["Lat"])
+        self.long = float(json["Lng"])
+        self.vvel = float(json["Vvel"])
+        self.e_category = int(json["Emitter_category"])
+        self.timestamp = dparser.parse(json["Timestamp"], fuzzy=True)
+
+    def print(self):
+        blog.debug("Addr: {}, Tail: {}, Alt: {} km, Speed: {} km/h, Lat: {}, Long: {}, Vvel: {}, Track: {}".format(self.addr, self.tail, self.alt, self.speed, self.lat, self.long, self.vvel, self.track))
+
+    def get_info_dict(self):
+        return {
+            "alt": self.alt,
+            "speed": self.speed,
+            "track": self.track,
+            "lat": self.lat,
+            "long": self.long,
+            "vvel": self.vvel,
+            "timestamp": str(self.timestamp)
+        }
+
+    def get_info_csv(self):
+        return "{};{};{};{};{};{};{}".format(self.alt, self.speed, self.track, self.lat, self.long, self.vvel, self.timestamp)
+
+class Plane:
+    def __init__(self, addr: int, tail: str):
+        self.addr = addr
+        self.tail = tail
+        self.entries = []
+        blog.info("New plane: addr: {}, tail: {}".format(self.addr, self.tail))
+
+    def add_entry(self, entry: Entry):
+        self.entries.append(entry)
+        blog.debug("Plane {} ({}) updated: Alt: {} m, Speed: {} m/s, Lat: {}, Long: {}".format(self.addr, self.tail, entry.alt, entry.speed, entry.lat, entry.long))
+
+        if (not os.path.exists("planes")):
+            os.makedirs("planes")
+
+        a = open("planes/{}.csv".format(self.tail), 'a')
+
+        a.writelines(entry.get_info_csv() + "\n")
+        a.close()
+
+class FlightDataThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.loop = asyncio.get_event_loop()
+        self.planes = []
+
+    def get_plane(self, addr: int):
+        for plane in self.planes:
+            if (plane.addr == addr):
+                return plane
+        return None
+
+    async def handler(self, websocket: websockets.WebSocketClientProtocol) -> None:
+        async for message in websocket:
+            jsn = json.loads(message)
+            entry = Entry(jsn)
+            plane = self.get_plane(entry.addr)
+            if (plane == None):
+                self.planes.append(Plane(entry.addr, entry.tail))
+            else:
+                plane.add_entry(entry)
+
+    async def consume(self, url: str) -> None:
+        async with websockets.connect(url) as websocket:
+            await self.handler(websocket)
+
+    def run(self):
+        self.loop.run_until_complete(self.consume("wss://ws.datapool.opendatahub.testingmachine.eu/flightdata/sbs-aggregated"))
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -31,6 +117,9 @@ if __name__ == "__main__":
             endpoints.branch_web_providers.get_get_providers())
     webserver.web_server.register_post_endpoints(
             endpoints.branch_web_providers.get_post_providers())
+
+    flightData = FlightDataThread()
+    flightData.start()
 
     # Start the webserver
     webserver.start_web_server("localhost", 8080)	
